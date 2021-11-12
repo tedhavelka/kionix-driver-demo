070f8ef16965274994d96cf8d628425e21072467
tedhavelka <ted@cs.pdx.edu>
2021-11-12 00:33:58 -0800

    WIP - to enable Zephyr thread analyzer API.

--- src/cli-zephyr-stack-info.c
+++ src/cli-zephyr-stack-info.c
@@ -1,21 +1,23 @@
+//----------------------------------------------------------------------
 //
-// File:  cli-zephyr-stack-info.c
+//  Project:  Kionix Driver Demo
 //
+//     File:  cli-zephyr-stack-info.c
 //
-//
+//----------------------------------------------------------------------
 
 
 #include <zephyr.h>         // . . .
 
 #include <stdint.h>         // to provide define of uint32_t
 
-//#include <include/debug/thread_analyzer.h>
-//#include <thread_analyzer.h>
-
-
-#include "../../zephyr/include/debug/thread_analyzer.h"
+// See project top level CMakeLists.txt file for `include_directory()` to provide relative
+// path of Zephyr thread analyzer header file:
+//#include <include/debug/thread_analyzer.h>  // <-- described as needed in Zephyr Doc' v2.7.99
+#include <thread_analyzer.h>
 
 #include "return-values.h"
+
 
 
 uint32_t cli__zephyr_2p6p0_stack_statistics(const char* args)
@@ -26,3 +28,7 @@ uint32_t cli__zephyr_2p6p0_stack_statistics(const char* args)
 
     return ROUTINE_OK;
 }
+
+
+
+// --- EOF ---
