c7faf9113facbe842dffeae265afdf6b46732366
Ted Havelka <thavelka@cornellpump.com>
2022-12-05 12:51:00 -0800

    Mods to local write_registers() routine, first SPI writes now working.

--- samples/kionix-trigger-work/src/main.c
+++ samples/kionix-trigger-work/src/main.c
@@ -10,22 +10,27 @@
 // - SECTION - defines
 //----------------------------------------------------------------------
 
-#define SLEEP_TIME_MS 500 // 200 // 4000 // 1000
+#define SLEEP_TIME_MS 2000
 #define DEFAULT_MESSAGE_SIZE 240
 
 // Demo / early development tests:
 #define DEV_TEST__FETCH_AND_GET_MANUFACTURER_ID    (1)
 #define DEV_TEST__FETCH_AND_GET_PART_ID            (0)
-#define DEV_TEST__FETCH_ACCELEROMETER_READINGS_XYZ (0)
+#define DEV_TEST__FETCH_ACCELEROMETER_READINGS_XYZ (1)
 
 // SPI related dev work:
 #define BUILD_FOR_SPI_CONNECTED_IIS2DH (1)
 
-#define DEV_SHOW_FIRST_BYTES_SPI_RX_BUFFER
+//#define DEV_SHOW_FIRST_BYTES_SPI_RX_BUFFER
 
-#define DEV_TEST__ENABLE_KX132_1211_ASYNCHRONOUS_READINGS (0)
+#define DEV_TEST__ENABLE_KX132_1211_ASYNCHRONOUS_READINGS      (0)
 #define DEV_TEST__KX132_ENABLE_SYNC_READINGS_WITH_HW_INTERRUPT (0)
-#define DEV_TEST__SET_KX132_1211_OUTPUT_DATA_RATE (0)
+#define DEV_TEST__SET_KX132_1211_OUTPUT_DATA_RATE              (0)
+
+#define SPI_MSBIT_AUTOINC_REG_ADDR_SET     1
+#define SPI_MSBIT_AUTOINC_REG_ADDR_CLEARED 0
+#define SPI_MSBIT_SET     SPI_MSBIT_AUTOINC_REG_ADDR_SET
+#define SPI_MSBIT_CLEARED SPI_MSBIT_AUTOINC_REG_ADDR_CLEARED
 
 
 
@@ -129,11 +134,11 @@ static void trigger_handler(const struct device *dev,
 
 
 
-static uint32_t read_registers(const struct device *dev,
-//                               const uint8_t device_address,
-                               const uint8_t* device_register,
-                               uint8_t* data,
-                               uint8_t len)
+static uint32_t read_registers(const struct device *dev,       // Zephyr pointer to peripheral or sensor device
+                               const uint8_t* device_register, // peripheral register address in small array
+                               uint8_t* data,                  // buffer to hold data we read from peripheral
+                               uint8_t len,                    // count of bytes to read from peripheral
+                               uint8_t option)                 // in SPI used to select auto-inc periph addr or not
 {
     uint8_t sensor_register_addr = device_register[0];
     const struct kx132_device_config *cfg = dev->config;
@@ -151,9 +156,19 @@ static uint32_t read_registers(const struct device *dev,
     rx_set.buffers = &rx_buf;
     rx_set.count = 1;
 
-    spi_tx_buffer[0] = (sensor_register_addr |= 0x80);  // logical OR with 0b10000000, read bit true, MS bit false.
+    if ( option == SPI_MSBIT_SET )
+    {
+        spi_tx_buffer[0] = (sensor_register_addr |= 0xC0);  // 0x80 = logical OR with 0b10000000, read bit true, MS bit true
+    }
+    else
+    {
+        spi_tx_buffer[0] = (sensor_register_addr |= 0x80);  // 0x80 = logical OR with 0b10000000, read bit true, MS bit false
+    }
+
     tx_buf.len = 2;
-    rx_buf.len = len;
+    rx_buf.len = (len + 1);
+
+printk("- read_registers() - ready to read %u bytes from register 0x%02x,\n", rx_buf.len, (spi_tx_buffer[0] & 0x3f));
 
     rstatus = spi_transceive(
                              cfg->spi.bus,
@@ -162,15 +177,82 @@ static uint32_t read_registers(const struct device *dev,
                              &rx_set
                             );
 
-//    *data = spi_rx_buffer[1];
-    printk("- INFO (read_registers) - ready to copy %u bytes to callers data buffer\n", len);
-//    memcpy(data, spi_rx_buffer[1], (unsigned int)len);
+printk("- read_registers() - after spi_transceive() call rx_buf holds { 0x%02X, 0x%02X }\n", spi_rx_buffer[0], spi_rx_buffer[1]);
+//    printk("- INFO (read_registers) - ready to copy %u bytes to callers data buffer\n", len);
     memcpy(data, &spi_rx_buffer[1], (size_t)len);
 
 #ifdef DEV_SHOW_FIRST_BYTES_SPI_RX_BUFFER
     printk("- DEV 1113 - first few bytes read back via spi_transceive():  0x%02x 0x%02x 0x%02x 0x%02x 0x%02x\n",
       spi_rx_buffer[0], spi_rx_buffer[1], spi_rx_buffer[2], spi_rx_buffer[3], spi_rx_buffer[4]);
 #endif
+
+    return rstatus;
+}
+
+
+
+static uint32_t write_registers(const struct device *dev,       // Zephyr pointer to peripheral or sensor device
+                               const uint8_t* device_register,  //
+                               uint8_t* data,                   // data to write to peripheral
+                               uint8_t len,                     // count of bytes to write
+                               uint8_t option)                  // in SPI used to select auto-inc periph addr or not
+{
+    uint8_t sensor_register_addr = device_register[0];
+    const struct kx132_device_config *cfg = dev->config;
+    uint32_t rstatus = 0;
+
+     spi_tx_buffer[0] = device_register[0];
+     spi_tx_buffer[1] = data[0];
+ 
+     tx_buf.len = 2; rx_buf.len = 2;
+
+// struct spi_buf_set rx_set = { .buffers = &rx_buf, .count = 1 };
+    rx_set.buffers = 0;
+    rx_set.count = 1;
+
+printk("- write_registers() - ready to write %u bytes to register 0x%02x,\n", tx_buf.len, (spi_tx_buffer[0] & 0x3f));
+printk("- write_registers() - tx_buf holds { 0x%02X, 0x%02X }\n", spi_tx_buffer[0], spi_tx_buffer[1]);
+
+    rstatus = spi_transceive(
+                             cfg->spi.bus,
+                             &cfg->spi.config,
+                             &tx_set,
+                             &rx_set
+                            );
+
+    return rstatus;
+}
+
+
+
+static uint32_t update_output_data_rate(const struct device *dev)
+{
+    uint32_t rstatus = 0;
+// These two local arrays get copied to file-scoped spi_tx_buff and spi_rx_buff arrays:
+    uint8_t data_to_write[2] = { 0, 0 };
+    uint8_t data_to_read[2] = { 0, 0 };
+
+
+    printk("- 1205 update_output_data_rate() - about to read KX132 register with addr 0x%02x . . .\n", KX132_ODCNTL);
+    data_to_read[0] = KX132_ODCNTL;
+    read_registers(dev, data_to_read, &data_to_read[1], 1, SPI_MSBIT_CLEARED);
+    printk("- 1205 update_output_data_rate() - ODR register 0x%02x holds %u\n", data_to_read[0], data_to_read[1]);
+
+    printk("- 1205 update_output_data_rate() - updating ODR register with value of 8 . . .\n");
+    data_to_write[0] = KX132_ODCNTL;
+    data_to_write[1] = 0x08;
+    write_registers(dev, data_to_write, &data_to_write[1], 1, SPI_MSBIT_CLEARED);
+
+    read_registers(dev, data_to_read, &data_to_read[1], 1, SPI_MSBIT_CLEARED);
+    printk("- 1205 update_output_data_rate() - ODR register 0x%02x now holds %u\n", data_to_write[0], data_to_read[1]);
+
+    printk("- 1205 update_output_data_rate() - updating ODR register with value of 6 . . .\n");
+    data_to_write[0] = KX132_ODCNTL;
+    data_to_write[1] = 0x06;
+    write_registers(dev, data_to_write, &data_to_write[1], 1, SPI_MSBIT_CLEARED);
+
+    read_registers(dev, data_to_read, &data_to_read[1], 1, SPI_MSBIT_CLEARED);
+    printk("- 1205 update_output_data_rate() - ODR register 0x%02x now holds %u\n", data_to_write[0], data_to_read[1]);
 
     return rstatus;
 }
@@ -275,17 +357,15 @@ void main(void)
         }
 
 #ifdef CONFIG_KX132_TRIGGER
-#warning "compiling KX132 trigger assignment code,"
-
-//	if (rc == 0) // may be some windowing or threshold setup to do before assigning values to trig structure - TMH
 	if ( 1 ) {
+            printk("- main.c - calling sensor_trigger_set() . . .\n");
             trig.type = SENSOR_TRIG_DATA_READY;
             trig.chan = SENSOR_CHAN_ACCEL_XYZ;
             rstatus = sensor_trigger_set(dev_kx132_1, &trig, trigger_handler);
-            printk("main.c, call to sensor_trigger_set() returns status value of %d\n", rstatus);
+            printk("- main.c - call to sensor_trigger_set() returns status value of %d\n", rstatus);
 
-            rstatus = kx132_trigger_set(dev_kx132_1, &trig, trigger_handler);
-            printk("main.c, call to kx132_trigger_set() returns status value of %d\n", rstatus);
+            rstatus |= kx132_trigger_set(dev_kx132_1, &trig, trigger_handler);
+            printk("- main.c - call to kx132_trigger_set() returns status value of %d\n", rstatus);
         }
 
         if ( rstatus != 0) {
@@ -300,15 +380,15 @@ void main(void)
 #define DT_DRV_COMPAT kionix_kx132_1211
 const struct gpio_dt_spec int_gpio_for_diag = GPIO_DT_SPEC_INST_GET_OR(0, drdy_gpios, { 0 }); // hmm, this results in correct name `&gpio1`
 
-    printk("- MARK 4 - main.c test of local gpio_dt_spec int_gpio.port->name . . .\n");
-    if ( int_gpio_for_diag.port != NULL )
-    { 
-        printk("- MARK 5 - main.c, interrupt GPIO port name holds '%s',\n", int_gpio_for_diag.port->name);
-    }
-    else
-    {
-        printk("- MARK 5 - main.c, interrupt GPIO port found NULL!\n");
-    }
+        printk("- MARK 4 - main.c test of local gpio_dt_spec int_gpio.port->name . . .\n");
+        if ( int_gpio_for_diag.port != NULL )
+        {
+            printk("- MARK 5 - main.c, interrupt GPIO port name holds '%s',\n", int_gpio_for_diag.port->name);
+        }
+        else
+        {
+            printk("- MARK 5 - main.c, interrupt GPIO port found NULL!\n");
+        }
 #endif // CONFIG_KX132_TRIGGER
     }
     else
@@ -338,6 +418,7 @@ IF_ENABLED(CONFIG_KX132_TRIGGER_NONE, ( \
 
 
 // - DEV 1128 - test of recently added port status data member:
+    if ( 0 )
     {
         const struct kx132_device_data *data = dev_kx132_1->data;
         printk("- DEV 1128 - main.c, driver gives drdy port status of %u\n", data->drdy_port_status);
@@ -362,6 +443,15 @@ IF_ENABLED(CONFIG_KX132_TRIGGER_NONE, ( \
             printk("- DEV 1128 - main.c, skipping reinit, drdy port already initialized\n");
         }
     }
+
+
+// - DEV 1205 --
+    printk("\n- DEV 1205 - main.c, testing stub for local SPI writes . . .\n");
+    rstatus = update_output_data_rate(dev_kx132_1);
+    printk("dev app exiting early . . .\n\n\n");
+    return;
+// ZZZZZ
+
 
 
     while ( 1 )
@@ -391,7 +481,7 @@ IF_ENABLED(CONFIG_KX132_TRIGGER_NONE, ( \
                     snprintf(lbuf, sizeof(lbuf), "0x%02X ", data_from_sensor.as_bytes[i]);
                     printk("%s", lbuf);
                 }
-#if 1
+#if 0
                 printk("\n");
 #else
                 printk("  \"");
@@ -400,7 +490,7 @@ IF_ENABLED(CONFIG_KX132_TRIGGER_NONE, ( \
                     snprintf(lbuf, sizeof(lbuf), " %c ", data_from_sensor.as_bytes[i]);
                     printk("%s", lbuf);
                 }
-                snprintf(lbuf, sizeof(lbuf), "\"\n");
+                snprintf(lbuf, sizeof(lbuf), "\"\n\n");
                 printk("%s", lbuf);
 #endif
             }
@@ -435,11 +525,46 @@ IF_ENABLED(CONFIG_KX132_TRIGGER_NONE, ( \
                 uint8_t *peripheral_reg_addr_ptr = per_addr;
                 char data[40] = {0};
                 char *data_ptr = data;
-#define FOUR_BYTES (4)
+#define FOUR_BYTES (4)  // length of KX132 manufacturer id string 'Kion'
+#define SIX_BYTES (6)   // count of bytes to hold 16 bit x, y, z accel readings
 
                 printk("- INFO (demo app) - testing SPI read registers routine:\n");
-                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, (FOUR_BYTES + 1));
+                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, (FOUR_BYTES + 1), SPI_MSBIT_CLEARED);
                 printk("- INFO (demo app) - over SPI bus read back manufacturer id string '%s'\n\n", data);
+
+                per_addr[0] = 8;
+                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, (SIX_BYTES + 1), SPI_MSBIT_CLEARED);
+                snprintf(lbuf, sizeof(lbuf), "main.c - x,y,z readings from local SPI read dev routine:  0x%04x, 0x%04x, 0x%04x\n",
+                  (data[0]*16+data[1]), (data[2]*16+data[3]), (data[4]*16+data[5]));
+                printk("%s", lbuf);
+
+                uint8_t readings[6] = {0,0,0,0,0,0};
+                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, 1, SPI_MSBIT_CLEARED);
+                readings[0] = data[0];
+
+                per_addr[0] = 9;
+                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, 1, SPI_MSBIT_CLEARED);
+                readings[1] = data[0];
+
+                per_addr[0] = 10;
+                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, 1, SPI_MSBIT_CLEARED);
+                readings[2] = data[0];
+
+                per_addr[0] = 11;
+                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, 1, SPI_MSBIT_CLEARED);
+                readings[3] = data[0];
+
+                per_addr[0] = 12;
+                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, 1, SPI_MSBIT_CLEARED);
+                readings[4] = data[0];
+
+                per_addr[0] = 13;
+                rstatus = read_registers(dev_kx132_1, peripheral_reg_addr_ptr, data_ptr, 1, SPI_MSBIT_CLEARED);
+                readings[5] = data[0];
+
+                snprintf(lbuf, sizeof(lbuf), "main.c - x,y,z readings via six SPI transactions:  0x%04x, 0x%04x, 0x%04x\n\n\n",
+                  (data[0]*16+data[1]), (data[2]*16+data[3]), (data[4]*16+data[5]));
+                printk("%s", lbuf);
             }
 
         } 
